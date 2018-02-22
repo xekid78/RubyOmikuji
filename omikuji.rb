9cfb961485d040b1ff81945790bd6c56ef219680
Makoto Tanabe <xekid78@outlook.com>
2018-02-22 19:37:58 +0900

    Add files via upload

---
+++ omikuji.rb
@@ -0,0 +1,14 @@
+omikuji = rand(1..6)
+if omikuji == 1
+  puts "大吉"
+elsif omikuji == 2
+  puts "中吉"
+elsif omikuji == 3
+  puts "小吉"
+elsif omikuji == 4
+  puts "吉"
+elsif omikuji == 5
+  puts "凶"
+elsif omikuji == 6
+  puts "大凶"
+end
